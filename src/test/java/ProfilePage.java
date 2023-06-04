2c41e21950368bdbb6fc955ca67f9729769fed24
Netti <47503740+marant22@users.noreply.github.com>
2023-06-04 19:42:36 +0200

    file upload test

--- src/test/java/ProfilePage.java
+++ src/test/java/ProfilePage.java
@@ -2,12 +2,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.LocalFileDetector;
+import java.util.concurrent.TimeUnit;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 class ProfilePage extends PageBase {
-    private By writeMottoBtn = By.xpath("//span[contains(@onclick, 'mottomodositas') and contains(@class, 'ikon32_49')]");
+    private By writeMottoBtn = By
+            .xpath("//span[contains(@onclick, 'mottomodositas') and contains(@class, 'ikon32_49')]");
     private By mottoTextarea = By.xpath("//textarea[@name='motto']");
     private By editMottoBtn = By.id("fa_sbmt");
     private By mottoContent = By.xpath("//div[@id='adatlap']//em");
+    private By fileUploadBtn = By.xpath("//a[contains(@onclick, 'adatlapkep.php')]");
+    private By fileInput = By.xpath("//input[@type='file']");
+    private By submitBtn = By.xpath("//button[@id='felhasz_kepek_sbmt']");
 
     public ProfilePage(WebDriver driver) {
         super(driver);
@@ -26,4 +36,36 @@ class ProfilePage extends PageBase {
 
         return motto;
     }
+
+    public void upload() {
+        // Assume driver is a RemoteWebDriver instance.
+        if (driver instanceof RemoteWebDriver) {
+            ((RemoteWebDriver) driver).setFileDetector(new LocalFileDetector());
+        }
+
+        WebElement btn = waitAndReturnElement(fileUploadBtn);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", btn);
+        btn.click();
+
+        File file = new File("src/test/IMG_20220227_231658.jpg");
+        String absolutePath = file.getAbsolutePath();
+
+        if (!file.exists()) {
+            System.out.println("File does not exist at " + absolutePath);
+            return;
+        }
+
+        WebElement input = waitAndReturnElement(fileInput);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", input);
+        input.sendKeys(absolutePath);
+        waitAndReturnElement(submitBtn).click();
+
+        // Wait for 10 seconds to observe the result
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -31,25 +31,24 @@ public class SeleniumTest {
 
     }
 
-    @Test
-    public void testLogin() {
-        // Test login functionality
-        MainPage mainPage = new MainPage(this.driver);
-        Assert.assertTrue(mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
-        LogedInPage logedinPage = mainPage.login("teszt98", "passwd");
-        Assert.assertTrue(logedinPage.isLogedIn());
-    }
+    // @Test
+    // public void testLogin() {
+    //     // Test login functionality
+    //     MainPage mainPage = new MainPage(this.driver);
+    //     Assert.assertTrue(mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
+    //     LogedInPage logedinPage = mainPage.login("teszt98", "passwd");
+    //     Assert.assertTrue(logedinPage.isLogedIn());
+    // }
 
-    @Test
-    public void testLogout() {
-        // Test logout functionality
-        MainPage mainPage = new MainPage(this.driver);
-        Assert.assertTrue(mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
-        LogedInPage logedinPage = mainPage.login("teszt98", "passwd");
-        mainPage = logedinPage.logout();
-        Assert.assertTrue(mainPage.isLogedOut());
-    }
-
+    // @Test
+    // public void testLogout() {
+    //     // Test logout functionality
+    //     MainPage mainPage = new MainPage(this.driver);
+    //     Assert.assertTrue(mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
+    //     LogedInPage logedinPage = mainPage.login("teszt98", "passwd");
+    //     mainPage = logedinPage.logout();
+    //     Assert.assertTrue(mainPage.isLogedOut());
+    // }
     @Test
     public void testProfileSetting() {
         MainPage mainPage = new MainPage(this.driver);
@@ -58,56 +57,59 @@ public class SeleniumTest {
         WebElement profileLink = logedinPage.waitAndReturnElement(By.xpath("//a[text()='Profilod']"));
         profileLink.click();
         WebDriverWait wait = new WebDriverWait(driver, 10); // Create WebDriverWait instance to wait for URL to change
-        wait.until(ExpectedConditions.urlToBe("https://www.hoxa.hu/adatlap-578160")); // Wait until the URL changes to
-                                                                                      // the profile page URL
+        wait.until(ExpectedConditions.urlToBe("https://www.hoxa.hu/adatlap-578160")); // Wait until the URL changes to the profile page URL
         ProfilePage profilePage = new ProfilePage(driver);
         String h1Text = profilePage.getH1Text();
         System.out.println(h1Text);
         Assert.assertTrue(h1Text.contains("Teszt98 adatlapja"));
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         String formattedDate = dateFormat.format(new Date());
         String text = "This is a test message " + formattedDate;
-        Assert.assertEquals(text, profilePage.WriteMotto(text));
-
+        String returnedText = profilePage.WriteMotto(text);
+        Assert.assertEquals(text.substring(0, 16), returnedText.substring(0, 16)); // compare up to the minute
+        profilePage.upload();
+        Assert.assertTrue(profilePage.isElementPresent(By.xpath("//span[contains(@class, 'ikon48_6') and contains(@onclick,'/jquery/profilkepnek.php?id=')]")));
     }
+    
+    
 
-    @Test
-    public void testSearch() {
-        // Test search functionality
-        MainPage mainPage = new MainPage(this.driver);
-        Assert.assertTrue(mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
-        SearchResultPage searchResultPage = mainPage.search("Teszt");
-        String bodyText = searchResultPage.getBodyText();
-        System.out.println(bodyText);
-        Assert.assertTrue(bodyText.contains("Teszt"));
-    }
+    // @Test
+    // public void testSearch() {
+    //     // Test search functionality
+    //     MainPage mainPage = new MainPage(this.driver);
+    //     Assert.assertTrue(mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
+    //     SearchResultPage searchResultPage = mainPage.search("Teszt");
+    //     String bodyText = searchResultPage.getBodyText();
+    //     System.out.println(bodyText);
+    //     Assert.assertTrue(bodyText.contains("Teszt"));
+    // }
 
-    @Test
-    public void testStaticPage() {
-        // Test static page navigation
-        MainPage mainPage = new MainPage(this.driver);
-        Assert.assertTrue(mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
-        WebElement recipeLink = mainPage.waitAndReturnElement(By.xpath("//a[text()='Receptek']"));
-        recipeLink.click();
-        String h1Text = mainPage.getH1Text();
-        System.out.println(h1Text);
-        Assert.assertTrue(h1Text.contains("Receptek"));
-    }
+    // @Test
+    // public void testStaticPage() {
+    //     // Test static page navigation
+    //     MainPage mainPage = new MainPage(this.driver);
+    //     Assert.assertTrue(mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
+    //     WebElement recipeLink = mainPage.waitAndReturnElement(By.xpath("//a[text()='Receptek']"));
+    //     recipeLink.click();
+    //     String h1Text = mainPage.getH1Text();
+    //     System.out.println(h1Text);
+    //     Assert.assertTrue(h1Text.contains("Receptek"));
+    // }
 
-    @Test
-    public void testStaticPages() throws IOException {
-        // Test loading multiple static pages from a configuration file
-        Properties config = loadConfigFile();
+    // @Test
+    // public void testStaticPages() throws IOException {
+    //     // Test loading multiple static pages from a configuration file
+    //     Properties config = loadConfigFile();
 
-        for (String key : config.stringPropertyNames()) {
-            String url = config.getProperty(key);
-            driver.get(url);
-            MainPage mainPage = new MainPage(driver);
-            Assert.assertTrue("Footer should contain expected text",
-                    mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
-            System.out.println("Page loaded successfully: " + url);
-        }
-    }
+    //     for (String key : config.stringPropertyNames()) {
+    //         String url = config.getProperty(key);
+    //         driver.get(url);
+    //         MainPage mainPage = new MainPage(driver);
+    //         Assert.assertTrue("Footer should contain expected text",
+    //                 mainPage.getFooterText().contains("2005-2023, www.hoxa.hu"));
+    //         System.out.println("Page loaded successfully: " + url);
+    //     }
+    // }
 
     private Properties loadConfigFile() throws IOException {
         Properties config = new Properties();
